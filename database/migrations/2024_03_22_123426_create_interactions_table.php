3cb45de500791cb551870945615a030092e1a3e1
Survivalx <goshaz7c@icloud.com>
2024-03-22 14:46:18 +0200

    migrate-tables-foreign-keys-learned

---
+++ database/migrations/2024_03_22_123426_create_interactions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('interactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('song_id')->constrained()->onDelete('cascade');
+            $table->tinyInteger('liked');
+            $table->integer('play_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('interactions');
+    }
+};
